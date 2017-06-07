73c86c35055cd559b995dd2e461620a976f369d3
Mohit Chattlani <chattlanimohit@gmail.com>
2017-06-07 22:30:17 +0530

    Days added in repay div

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	var checkdate=14;
-	var days={Sun:[18,25,02]};
 	$('.button').click(function(){
 		$('.button').css('border-color','transparent')
 		$(this).css('border-color','#4494e5');
-		var display="Repay on";
+		var display="Repay on ";
 		var clickdate=$(this).html();
 		if (clickdate>checkdate) {
-			display=display+' June '+clickdate;
+			display=display+"<b>"+$(this).val()+', June '+clickdate+"</b>";
 		}
 		else
 		{
-			display=display+' July '+clickdate;
+			display=display+"<b>"+$(this).val()+', July '+clickdate+"</b>";
 		}
 		console.log(display);
+		$('#repay').html(display);
 	});
 
 });
