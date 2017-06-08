0a3c9d3c6d11a1939d967f7535aa33d5fd1fd192
Mohit Chattlani <chattlanimohit@gmail.com>
2017-06-09 02:38:39 +0530

    Completed slider functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	var checkdate=14;
+	$('#slider').on('input',function(){
+		$('.display').html('&#36;'+$(this).val());
+	});
 	$('.button').click(function(){
 		$('.button').css('border-color','transparent')
 		$(this).css('border-color','#4494e5');
