6d7af604ead13bfb6d47a124500b5da7b9e597e0
Mohit Chattlani <chattlanimohit@gmail.com>
2017-06-09 04:09:34 +0530

    Mostly styling changes

--- script.js
+++ script.js
@@ -6,17 +6,22 @@ $(document).ready(function(){
 	$('.button').click(function(){
 		$('.button').css('border-color','transparent')
 		$(this).css('border-color','#4494e5');
-		var display="Repay on ";
+		var display="";  //display string at top 
+		var display2="on ";        //display string at bottom-right 
 		var clickdate=$(this).html();
 		if (clickdate>checkdate) {
 			display=display+"<b>"+$(this).val()+', June '+clickdate+"</b>";
+			display2=display2+'June '+clickdate+",2017";
 		}
 		else
 		{
 			display=display+"<b>"+$(this).val()+', July '+clickdate+"</b>";
+			display2=display2+'July '+clickdate+",2017";
 		}
 		console.log(display);
+		console.log(display2);
 		$('#repay').html(display);
+		$('#lowright').html(display2);
 	});
 
 });
