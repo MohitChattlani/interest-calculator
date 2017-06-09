960fe7e97c3c416fcec0af030cc1778eb7c75e80
Mohit Chattlani <chattlanimohit@gmail.com>
2017-06-09 15:42:20 +0530

    Calculated and printed interest, repayment value and things therein,some styling changes also.

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 $(document).ready(function(){
-	var checkdate=14;
 	$('#slider').on('input',function(){
-		$('.display').html('&#36;'+$(this).val());
+		var amount=$(this).val();
+		var interest=amount*0.176;
+		interest=interest.toFixed(2);
+		var total=parseFloat(amount)+parseFloat(interest);
+		total=total.toFixed(2);
+		$('.display').html('&#36;'+amount);
+		$('.interest').html('&#36;'+interest);
+		$('.repayment').html('&#36;'+total);
 	});
 	$('.button').click(function(){
 		$('.button').css('border-color','transparent')
 		$(this).css('border-color','#4494e5');
-		var display="";  //display string at top 
-		var display2="on ";        //display string at bottom-right 
 		var clickdate=$(this).html();
-		if (clickdate>checkdate) {
-			display=display+"<b>"+$(this).val()+', June '+clickdate+"</b>";
-			display2=display2+'June '+clickdate+",2017";
+		var dateObj = new Date();
+		var day = dateObj.getUTCDate();    //getting currentdate
+		var daysleft=clickdate-day-1;
+		var display="";                    //display string at top 
+		var display2="on ";
+		var display3="for "+daysleft+" days";                //display string at bottom-right 
+		if (clickdate>day) {
+			display=display+"<b>"+$(this).val()+', Jun '+clickdate+"</b>";
+			display2=display2+'Jun '+clickdate+",2017";
 		}
 		else
 		{
-			display=display+"<b>"+$(this).val()+', July '+clickdate+"</b>";
-			display2=display2+'July '+clickdate+",2017";
+			display=display+"<b>"+$(this).val()+', Jul '+clickdate+"</b>";
+			display2=display2+'Jul '+clickdate+",2017";
 		}
-		console.log(display);
-		console.log(display2);
 		$('#repay').html(display);
 		$('#lowright').html(display2);
+		$('#lowleft').html(display3);
 	});
 
 });
