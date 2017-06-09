2d3b07df673fe5aa812a425796cd24be31800b48
Mohit Chattlani <chattlanimohit@gmail.com>
2017-06-09 21:30:14 +0530

    Adding comments for better readability

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('#slider').on('input',function(){
 		var amount=$(this).val();
 		var interest=amount*0.176;
-		interest=interest.toFixed(2);
+		interest=interest.toFixed(2);       //convert into 2 decimal places
 		var total=parseFloat(amount)+parseFloat(interest);
 		total=total.toFixed(2);
 		$('.display').html('&#36;'+amount);
@@ -10,18 +10,18 @@ $(document).ready(function(){
 		$('.repayment').html('&#36;'+total);
 	});
 	$('.button').click(function(){
-		$('.button').css('border-color','transparent')
-		$(this).css('border-color','#4494e5');
+		$('.button').css('border-color','transparent');   //making all buttons transparent border
+		$(this).css('border-color','#4494e5');			  //then selected button is bordered blue
 		var clickdate=$(this).html();
 		var dateObj = new Date();
 		var day = dateObj.getUTCDate();    //getting currentdate
 		var daysleft;
 		var display="";                    //display string at top 
-		var display2="on "; 
+		var display2="on ";                
 		if (clickdate>day) {
 			display=display+"<b>"+$(this).val()+', Jun '+clickdate+"</b>";
 			display2=display2+'Jun '+parseInt(clickdate)+",2017";
-			daysleft=clickdate-day-1;
+			daysleft=parseInt(clickdate)-day-1;
 		}
 		else
 		{
